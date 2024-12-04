c66e2754ba4fa07816f4c7260dca152dc1fd8c8a
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-12-04 23:08:40 +0800

    Add project files.

---
+++ DataSet1.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP_System
+{
+
+
+    partial class DataSet1
+    {
+        partial class dtItemOrderDataTable
+        {
+        }
+
+        partial class dtStockInDataTable
+        {
+        }
+
+        partial class dtInventoryDataTable
+        {
+        }
+
+        partial class dtSoldItemsDataTable
+        {
+        }
+
+        partial class dtTopSellingDataTable
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_System
+{
+    internal static class Program
+
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmPOS(null, null));
+            //Application.Run(new frmSecurity());
+            //Application.Run(new Form1());
+            //Application.Run(new QRCodeGenerator());
+            Application.Run(new mj_mainForm());
+
+        }
+    }
+}
